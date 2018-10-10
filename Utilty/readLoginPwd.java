3125beaa923ab576fdcefb103d2608b96a4e1f90
Srijan <sshakya@sevadev.com>
2018-10-10 18:13:31 +0545

    read login utility made

--- Utilty/readLoginPwd.java
+++ Utilty/readLoginPwd.java
@@ -1,9 +1,32 @@
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Webdriver;
+import org.openqa.selenium.WebElement;
+
 public class readLoginPwd {
 
-	public static void main(String[] args) {
+	private static WebElement element = null;
+	
+	public static WebElement EmailAddress(WebDriver driver) {
 		// TODO Auto-generated method stub
-
+			element email = driver.findElement(By.xpath("//*[@id=\"UserName\"]"));
+			
+			return element;
 	}
-
+	
+	public static WebElement Password(WebDriver driver) {
+			element password = driver.findElement(By.xpath("//*[@id=\"Password\"]"));
+			
+			return element;
+	}
+	
+	public static WebElement Login(WebDriver driver) {
+		
+			element login = driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div[3]/input"));
+			
+			return element;
+	}
+	
 }
+ 
