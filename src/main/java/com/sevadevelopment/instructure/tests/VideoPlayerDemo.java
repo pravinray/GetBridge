6e1495f397087aab5f96058dcc787079cb00a6a6
Pawan Krishna Shrestha <pkshrestha@sevadev.com>
2018-10-30 09:30:37 +0545

    Added config utility; now every testcase opens new browser

--- src/main/java/com/sevadevelopment/instructure/tests/VideoPlayerDemo.java
+++ src/main/java/com/sevadevelopment/instructure/tests/VideoPlayerDemo.java
@@ -1,49 +1,32 @@
 package com.sevadevelopment.instructure.tests;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
-
+import com.sevadevelopment.utility.ConfigUtility;
+import com.sevadevelopment.utility.SeleniumDriverFactory;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import com.sevadevelopment.utility.SeleniumDriverFactory;
-
 public class VideoPlayerDemo {
 	WebDriver driver;
 	Screen s = new Screen();
 	Pattern image = new Pattern("src/main/resources/sikuliImages/1540383765278.png");
+	ConfigUtility configUtility;
 
 	@BeforeClass
-	public void setupTestClass() throws InterruptedException {
-		Properties properties = new Properties();
-		FileInputStream fileInputStream = null;
-		try {
-			fileInputStream = new FileInputStream("src/main/resources/config.properties");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			properties.load(fileInputStream);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		driver = new SeleniumDriverFactory().getDriver(properties.getProperty("browser"));
-		driver.manage().window().setSize(new Dimension(1024, 768));
+	public void setupTestClass() {
+		configUtility = new ConfigUtility();
 	}
 
 	@BeforeMethod
 	public void setupTestMethod() {
+		driver = new SeleniumDriverFactory().getDriver(configUtility.getConfig("browser"));
+		driver.manage().window().setSize(new Dimension(1024, 768));
 		driver.get("https://www.getbridge.com/?lead_source_description=instructure.com_");
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0, 400)", "");
@@ -52,10 +35,6 @@ public class VideoPlayerDemo {
 	@AfterMethod
 	public void tearDownTestMethod() {
 		driver.manage().deleteAllCookies();
-	}
-
-	@AfterClass
-	public void tearDownTestClass() {
 		driver.quit();
 	}
 
